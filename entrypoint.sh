3cd9e49a811a3de36bd790ab6eccf1663940749c
StateraMentis <statera0mentis@gmail.com>
2026-07-31 11:12:37 +0700

    Исправил битую кодировку (сбилась при клонировании)

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,4 @@ else
 fi
 
 echo "[ ] Запуск main:app..."
-exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000                   
+exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
